0baf707189a802f510faf4888d10df8a32e52b0b
Chorn Thorn <chornthorn.kh@gmail.com>
2023-11-05 23:50:15 +0700

    feat: Add category feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, CategoryModule],
 })
 export class AppModule {}
